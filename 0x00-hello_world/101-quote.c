b4e38bca640eba1bbe848d2f5e12c0758f266bc3
AAabbabbado <bdh211884@gmail.com>
2023-03-16 18:44:09 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,15 @@
-#include <nistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Description: print a qoute using write function
- *	write(int fd.const void *buf.size_t count)
+ * size_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (not success)
+ * Return: 1 (error).
 */
-
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(1.quo.59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
 	return (1);
 }
