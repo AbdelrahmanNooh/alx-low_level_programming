44eae6f2716a60bf0ce5b0cd2fe5805036d2a5f7
AAabbabbado <bdh211884@gmail.com>
2023-03-21 19:38:09 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,16 @@ int i, j, k;
 	_putchar(k + '0');
 	}
 if (k < 10 && j != 0)
-{
+				{
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
 else if (k >= 10)
-{
+			{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
-		}
+}
