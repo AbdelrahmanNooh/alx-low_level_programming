177037a773a70ac1c76379f1db06c6b5f42d90e5
AAabbabbado <bdh211884@gmail.com>
2023-04-03 12:33:37 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,28 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
+
 /**
- *_strstr - locates a substring
- *@haystack: string
- *@needle: string
- *Return: pointer to the beginning of the located subtring or
- *NULL if the substring is not found
+ * _strstr - Entry point
+ * @haystack: input
+ * @need: input
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int con1, con2, con3;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
 
-	for (con1 = 0; haystack[con1] != '\0'; con1++)
-		if (needle[con2] != haystack[con3] || haystack[con3] == '\0')
+		while (*one == *two && *two != '\0')
 		{
-			break;
+			one++;
+			two++;
 		}
-}
-if (needle[con2] == '\0')
-{
-	return (haystack + con1);
-}
-}
-return (NULL);
+
+		if (*two == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
 }
