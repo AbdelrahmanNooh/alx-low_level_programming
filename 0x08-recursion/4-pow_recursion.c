aafa0f9206cfe8266a13e98b73e671f524f36dcd
AAabbabbado <bdh211884@gmail.com>
2023-04-05 17:21:33 -0700

    a

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
+ * _pow_recursion - search a string for any of a set of bytes
  * @x: base
  * @y: exposant
- * Return: pointer to the byte in s that matches one of the bytes in accept or NULL if no such byte is found
+ * Return: pointer to the byte in s that matches one of the bytes in accept or NULL if no such byte is found.
  */
 
 int _pow_recursion(int x, int y)
