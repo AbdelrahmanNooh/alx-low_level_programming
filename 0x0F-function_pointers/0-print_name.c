c7b8fc6b58e9a1b18981843d5c3cc0d24df36c8f
AAabbabbado <bdh211884@gmail.com>
2023-04-19 19:40:23 -0700

    a

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - Print a name from a function pointer
- *
- * @name: The string name function
- *
- * @f: function pointer that takes a string argument
- */
-
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,19 +1,21 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * array_iterator - execute a function pointer on each element of an array
- *
- * @array: array of integers
- *
- * @size: size of array
- *
- * @action: function pointer
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
+ * Return: void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
+	{
 		action(array[i]);
+	}
 }
