29227095469476ad1cadfa7a16a762267df7808e
AAabbabbado <bdh211884@gamil.com>
2023-05-02 16:42:16 -0700

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,13 +1,11 @@
 #include "lists.h"
 
 /**
- * free_listint2 - Entry point
- *
+ * free_listint2 - frees a list of ints
  * @head: address of pointer to first node
  *
- * return: void
+ * Return: void
  */
-
 void free_listint2(listint_t **head)
 {
 	listint_t *node, *teap;
@@ -20,7 +18,7 @@ void free_listint2(listint_t **head)
 	{
 		teap = node;
 		node = node->next;
-		free(t);
+		free(teap);
 	}
 	*head = NULL;
 }
