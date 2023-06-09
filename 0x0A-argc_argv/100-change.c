fe07148853cad9c058401e478e2728b01e3e9ac3
AbdelrahmanNooh <bdh211884@gmail.com>
2023-06-09 11:20:55 -0700

    a

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
-
 /**
- * main - function to print out the change that needs to given
- * @argc: number of arguments to be passed
- * @argv: number of variables to calculate
- * Return: always 0 for sucess
+ * main - prints the minimum number of coins to make change money
+ * @argc: count
+ * @argv: array
+ * Return: 0
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int total, change;
+	int coin;
+	int can;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	change = atoi(argv[1]);
-
-	for (total = 0; change > 0; total++)
+	coin = atoi(argv[1]);
+	can = 0;
+	while (coin > 0)
 	{
-		if (change - 25 >= 0)
-			change = change - 25;
-		else if (change - 10 >= 0)
-			change = change - 10;
-		else if (change - 5 >= 0)
-			change = change - 5;
-		else if (change - 2 >= 0)
-			change = change - 2;
-		else if (change - 1 >= 0)
-			change = change - 1;
+		if (coin >= 25)
+		{
+			coin -= 25;
+			can += 1;
+		}
+		else if (coin >= 10)
+		{
+			coin -= 10;
+			can += 1;
+		}
+		else if (coin >= 5)
+		{
+			coin -= 5;
+			can += 1;
+		}
+		else if (coin >= 2)
+		{
+			coin -= 2;
+			can += 1;
+		}
+		else
+		{
+			coin -= 1;
+			can += 1;
+		}
 	}
-	printf("%d\n", total);
+	printf("%d\n", can);
 	return (0);
 }
